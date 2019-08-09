f9e61cb9ff4bc8c1d3ef66ad21f42bd68ff0ec9d
yamam00s <s.yamamo0327@gmail.com>
2019-08-10 00:56:34 +0900

    コンポーネント使うときはケバブケースに

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,39 @@
 <template>
   <div class="container">
-    <TgCharacter :is-live="isLive" />
+    <tg-button>button</tg-button>
+    <tg-character :is-live="isLive" />
+    <tg-balloon>
+      <p>
+        aaaa
+      </p>
+    </tg-balloon>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import { Getter, Mutation } from 'vuex-class'
-import TgCharacter from '~/components/atoms/TgCharacter.vue'
-
-const sleep = (sec): Promise<number> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, sec)
-  })
-}
+import TgCharacter from '~/components/atoms/character/TgCharacter.vue'
+import TgBalloon from '~/components/atoms/balloon/TgBalloon.vue'
+import TgButton from '~/components/atoms/button/TgButton.vue'
 
 @Component({
   components: {
-    TgCharacter
+    TgCharacter,
+    TgBalloon,
+    TgButton
   }
 })
 export default class App extends Vue {
   @Mutation('setLive') setLive
   @Getter('isLive') isLive
-
-  async mounted() {
-    await sleep(10000)
-    this.setLive(false)
-  }
 }
 </script>
 
 <style>
 .container {
+  width: 100%;
+  max-width: 1280px;
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
