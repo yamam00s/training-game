bb5a3f684f3dac40c0a85d755101bb3abec00897
yamam00s <s.yamamo0327@gmail.com>
2019-08-13 00:42:28 +0900

    create box

---
+++ components/atoms/box/TgBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class TgBox extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  border: 10px double #000;
+  background: #fff;
+  text-align: center;
+}
+</style>
