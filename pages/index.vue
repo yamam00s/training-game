b71396a529952e89dde337bd9e8aad0932ac1902
yamam00s <s.yamamo0327@gmail.com>
2019-08-14 23:33:46 +0900

    header適応

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h1>まりも</h1>
-    <tg-button>button</tg-button>
+    <tg-header title="ただのまりも" @button-click-1="buttonClick"></tg-header>
     <div class="container">
       <tg-box>
         <tg-character :is-live="isLive" />
@@ -20,20 +19,24 @@ import { Vue, Component } from 'vue-property-decorator'
 import { Getter, Mutation } from 'vuex-class'
 import TgCharacter from '~/components/atoms/character/TgCharacter.vue'
 import TgBalloon from '~/components/atoms/balloon/TgBalloon.vue'
-import TgButton from '~/components/atoms/button/TgButton.vue'
 import TgBox from '~/components/atoms/box/TgBox.vue'
+import TgHeader from '~/components/organisms/header/TgHeader.vue'
 
 @Component({
   components: {
     TgCharacter,
     TgBalloon,
-    TgButton,
-    TgBox
+    TgBox,
+    TgHeader
   }
 })
 export default class App extends Vue {
   @Mutation('setLive') setLive
   @Getter('isLive') isLive
+
+  private buttonClick() {
+    console.log('aa')
+  }
 }
 </script>
 
