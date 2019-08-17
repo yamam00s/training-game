6927ca64dc7ede45578fb907566a9b3c3997872c
yamam00s <s.yamamo0327@gmail.com>
2019-08-18 03:30:44 +0900

    firebase init

---
+++ plugins/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcSbju_fsvhpm9BkTRnbklzNvYPXpkCGI',
+  authDomain: 'training-game-12576.firebaseapp.com',
+  databaseURL: 'https://training-game-12576.firebaseio.com',
+  projectId: 'training-game-12576',
+  storageBucket: 'training-game-12576.appspot.com',
+  messagingSenderId: '890930690403',
+  appId: '1:890930690403:web:035c6e914a81b319'
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
