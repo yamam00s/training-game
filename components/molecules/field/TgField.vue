935c2aab33feae4451093a8b44013efaeee66192
yamam00s <s.yamamo0327@gmail.com>
2019-08-26 00:16:11 +0900

    update TgField

--- components/molecules/field/TgField.vue
+++ components/molecules/field/TgField.vue
@@ -1,31 +1,19 @@
 <template>
   <div>
     <label>
-      <p class="label"><slot /></p>
-      <tg-input :placeholder="placeholder" @input="input"></tg-input>
+      <p class="label">{{ label }}</p>
+      <slot />
     </label>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Prop, Component } from 'vue-property-decorator'
-import TgInput from '~/components/atoms/input/TgInput.vue'
 
-@Component({
-  components: {
-    TgInput
-  }
-})
+@Component({})
 export default class TgField extends Vue {
   @Prop({})
-  placeholder!: string
-
-  value: string = ''
-
-  input(value) {
-    this.value = value
-    console.log(value)
-  }
+  label!: string
 }
 </script>
 
