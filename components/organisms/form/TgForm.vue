056d965e5346082dcd73826a7b68f820ada72b7f
yamam00s <s.yamamo0327@gmail.com>
2019-09-01 20:28:05 +0900

    create TgForm

--- components/organisms/form/TgForm.vue
+++ components/organisms/form/TgForm.vue
@@ -1,33 +1,34 @@
 <template>
-  <div class="container">
-    <form></form>
+  <div>
+    <form>
+      <tg-field label="test">
+        <tg-input
+          v-model="value"
+          name="test"
+          placeholder="placeholder"
+        ></tg-input>
+      </tg-field>
+      <!-- <tg-button @button-click="formSubmit">登録</tg-button> -->
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Prop, Component } from 'vue-property-decorator'
-// import TgField from '~/components/molecules/field/TgField.vue'
-@Component({
-  // components: {
-  //   TgField
-  // }
-})
-export default class Form extends Vue {
-  @Prop({})
-  value!: string
+import { Vue, Component } from 'vue-property-decorator'
+import TgInput from '~/components/atoms/input/TgInput.vue'
+import TgButton from '~/components/atoms/button/TgButton.vue'
+import TgField from '~/components/molecules/field/TgField.vue'
 
-  update(key: string, value: string) {
-    this.value[key] = value
-    this.$emit('input', this.value)
+@Component({
+  components: {
+    TgButton,
+    TgInput,
+    TgField
   }
+})
+export default class TgForm extends Vue {
+  value: string = ''
 }
 </script>
 
-<style lang="scss" scoped>
-.container {
-  width: 400px;
-  height: 400px;
-  flex-wrap: wrap;
-  text-align: left;
-}
-</style>
+<style lang="scss" scoped></style>
