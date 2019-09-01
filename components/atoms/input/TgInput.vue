9a6323880bc3555e314e3f135eddb5ed5f749d60
yamam00s <s.yamamo0327@gmail.com>
2019-09-01 22:18:26 +0900

    update Tginput textateaとtype=fileに対応

--- components/atoms/input/TgInput.vue
+++ components/atoms/input/TgInput.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     v-if="type !== 'textarea'"
-    class="input"
+    :class="type !== 'file' ? 'input' : 'upload'"
     :type="type"
     :name="name"
     :placeholder="placeholder"
@@ -9,6 +9,7 @@
     v-bind="$attrs"
     @input="$emit('input', $event.target.value)"
   />
+
   <textarea
     v-else
     class="textarea"
@@ -44,5 +45,27 @@ export default class TgInput extends Vue {
   border: 1px solid transparent;
   border-radius: 4px;
   line-height: 1.6;
+}
+
+.textarea {
+  width: 100%;
+  font-size: 1.6rem;
+  height: 7rem;
+  padding: calc(0.375em - 1px) calc(0.625em - 1px);
+  border: 1px solid transparent;
+  border-radius: 4px;
+  line-height: 1.6;
+}
+
+.upload {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  outline: none;
+  cursor: pointer;
+  z-index: -1;
 }
 </style>
